8755d5edb8d765b901d56c40a74648b7825f934a
ketdevbanban <125855289+ketdevbanban@users.noreply.github.com>
2023-04-17 16:52:54 +0700

    edit env

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center bg-green-800 h-[70px] text-white font-Noto shadow-xl">
       <div className="text-3xl font-semibold text-gray-200">
-        Ket Dev Ban Ban
+        <p>ທີມງານສ້ອມແປງເຄື່ອງຈັກ</p>
       </div>
       <div className="w-[600px] items-center flex text-center  bg-white rounded-lg p-2">
         <SearchOutlined style={{ fontSize: '16px', color: '#08c' ,marginLeft: '10px' ,cursor:'pointer'}}/>
